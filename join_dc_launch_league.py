625234097eb5010f92c0aeaa30445d7d61b95584
violetscribbles <83583049+violetscribbles@users.noreply.github.com>
2021-05-22 17:41:39 +1000

    Update join_dc_launch_league.py

--- join_dc_launch_league.py
+++ join_dc_launch_league.py
@@ -1,10 +1,13 @@
+# This script uses webbrowser.open and os.startfile to streamline my gaming experiences. 
+# It opens League of Legends, Discord, u.gg and op.gg at the same time.
+
 import os, webbrowser
 
 def launch_apps(league, disco):
     os.startfile(league) # launches league
     os.startfile(disco) # launches discord
 
-launch_apps('D:\\Games\League of Legends\LeagueClient.exe', 'C:\\Users\jackc\Desktop\Discord')
+launch_apps('D:\\Games\League of Legends\LeagueClient.exe', 'C:\\Users\-REDACTED-\Desktop\Discord') # redacted personal directory
 
 webbrowser.open("https://u.gg/", new=2) # opens u.gg in new tab
-webbrowser.open("https://oce.op.gg/", new=2) # opens op.gg
+webbrowser.open("https://oce.op.gg/", new=2) # opens op.gg in a new tab
