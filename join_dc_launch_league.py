0ed86990f961d0d608e55a72d4221f148854cac4
violetscribbles <83583049+violetscribbles@users.noreply.github.com>
2021-05-22 17:39:12 +1000

    Add files via upload

---
+++ join_dc_launch_league.py
@@ -0,0 +1,10 @@
+import os, webbrowser
+
+def launch_apps(league, disco):
+    os.startfile(league) # launches league
+    os.startfile(disco) # launches discord
+
+launch_apps('D:\\Games\League of Legends\LeagueClient.exe', 'C:\\Users\jackc\Desktop\Discord')
+
+webbrowser.open("https://u.gg/", new=2) # opens u.gg in new tab
+webbrowser.open("https://oce.op.gg/", new=2) # opens op.gg
